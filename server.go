d9192afe36567667b56ef65d4931ddbf2741a99f
noamt <noam@10ne.org>
2016-02-15 20:07:08 +0200

    Completing basic subject persistence method

--- server.go
+++ server.go
@@ -15,28 +15,22 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func (ph *PersistenceHandler) CreateSubject(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Printf("Received request to create subject: %s \n", p.ByName("subject"))
-	decoder := json.NewDecoder(r.Body)
-
-	var s Subject
-	err := decoder.Decode(&s)
+	s, err := decodeSubject(r)
+	s.Username = p.ByName("subject")
 
 	if err != nil {
-		fmt.Printf("Error decoding JSON payload: %v", err)
-		fmt.Fprint(w, "Error decoding JSON payload")
-		w.WriteHeader(http.StatusBadRequest)
+		handleDecodingError(w, err)
 		return
 	}
-	fmt.Printf("Decoded requst body with email: %s \n", s.email)
-	subject := &persistence.Subject{Username: p.ByName("subject"), Email: s.email}
-	err = ph.PS.CreateSubject(subject)
+
+	err = persistSubject(s, ph.PS)
 	if err != nil {
 		fmt.Printf("Error persisting new subject: %v", err)
-		fmt.Fprint(w, "Error persisting new subject")
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error occurred while creating the subject"))
 		return
 	}
-	fmt.Fprint(w, "Successfully created subject")
-	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Subject created successfully"))
 }
 
 func (ph *PersistenceHandler) Projects(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -66,10 +60,6 @@ func (ph *PersistenceHandler) UpdateDocument(w http.ResponseWriter, r *http.Requ
 func (ph *PersistenceHandler) DeleteDocument(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
-type Subject struct {
-	email string
-}
-
 type PersistenceHandler struct {
 	PS *persistence.PersistenceSession
 }
@@ -95,4 +85,30 @@ func main() {
 	router.DELETE("/document/:subject/:project/*document", persistenceHandler.DeleteDocument)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+type Subject struct {
+	Username string
+	Email    string
+}
+
+func decodeSubject(r *http.Request) (Subject, error) {
+	defer r.Body.Close()
+	decoder := json.NewDecoder(r.Body)
+
+	var s Subject
+	err := decoder.Decode(&s)
+	return s, err
+}
+
+func persistSubject(s Subject, ps *persistence.PersistenceSession) error {
+	subject := &persistence.Subject{Username: s.Username, Email: s.Email}
+	err := ps.CreateSubject(subject)
+	return err
+}
+
+func handleDecodingError(w http.ResponseWriter, err error) {
+	fmt.Printf("Error decoding JSON payload: %v", err)
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintf(w, "Error decoding JSON payload: %v", err)
 }
